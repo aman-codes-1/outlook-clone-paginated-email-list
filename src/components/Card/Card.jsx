4bee2cd31092a35f445c060d8a5a1262349b5e13
Aman Jain <aman.codes0@gmail.com>
2023-11-05 03:07:06 +0530

    card-loader

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,35 +1,29 @@
-import { formatDate } from "../../helpers";
 import "./Card.css";
 
-const Card = ({ item, id, emailCard, handleClick }) => {
+const Card = ({
+  item,
+  id,
+  avatar,
+  from,
+  subject,
+  shortDesc,
+  date,
+  handleClick,
+}) => {
   return (
     <div
-      className={`card ${item?.id === id ? "card-read" : ""}`}
+      className={`card ${item && id && item?.id === id ? "card-read" : ""}`}
       onClick={(_) => handleClick(_, item?.id)}
     >
       <div className="card-container">
         <div className="left-container">
-          <div className="avatar">
-            {item?.from?.name?.substring(0, 1).toUpperCase()}
-          </div>
+          {avatar}
         </div>
         <div className="right-container">
-          <div>
-            From:{" "}
-            <strong
-              style={{ wordWrap: "break-word" }}
-            >{`${item?.from?.name} <${item?.from?.email}>`}</strong>
-          </div>
-          <div>
-            Subject:{" "}
-            <strong style={{ wordWrap: "break-word" }}>{item?.subject}</strong>
-          </div>
-          {item?.short_description ? (
-            <div className="card-short-description">
-              {emailCard ? item?.short_description?.substring(0, 48) + "..." : item?.short_description}
-            </div>
-          ) : null}
-          <div>{item?.date ? formatDate(item?.date) : null}</div>
+          {from}
+          {subject}
+          {shortDesc}
+          {date}
         </div>
       </div>
     </div>
--- src/components/CardLayout/CardLayout.jsx
+++ src/components/CardLayout/CardLayout.jsx
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react";
-import { Card, CardBody } from "..";
-import { callApi } from "../../helpers";
+import { Card, CardBody, CardLoader } from "..";
+import { callApi, formatDate } from "../../helpers";
 import "./CardLayout.css";
-import { Alert } from "@mui/material";
 
 const CardLayout = ({ data, loadingComp, emailCard, setEmailCard }) => {
   const [state, setState] = useState({
@@ -53,30 +52,58 @@ const CardLayout = ({ data, loadingComp, emailCard, setEmailCard }) => {
   };
 
   return (
-    <>
-      {loadingComp ? (
-        <Alert sx={{ mr: "25px" }} severity="warning" variant="standard">
-          Loading...
-        </Alert>
-      ) : null}
-      <div className={`card-layout ${emailCard ? "card-selected" : ""}`}>
-        <div
-          className={`card-wrapper ${emailCard ? "card-wrapper-selected" : ""}`}
-        >
-          {data?.length
-            ? data.map((item) => (
-                <Card
-                  item={item}
-                  id={id}
-                  emailCard={emailCard}
-                  handleClick={handleClick}
-                />
-              ))
-            : null}
-        </div>
-        {emailCard ? <CardBody data={data} state={state} id={id} /> : null}
+    <div className={`card-layout ${emailCard ? "card-selected" : ""}`}>
+      <div
+        className={`card-wrapper ${emailCard ? "card-wrapper-selected" : ""}`}
+      >
+        {loadingComp &&
+          Array(10)
+            .fill(0)
+            .map(() => <CardLoader />)}
+        {!loadingComp && data?.length
+          ? data.map((item) => (
+              <Card
+                item={item}
+                id={id}
+                avatar={
+                  <div className="avatar">
+                    {item?.from?.name?.substring(0, 1).toUpperCase()}
+                  </div>
+                }
+                from={
+                  <div>
+                    From:{" "}
+                    <strong
+                      style={{ wordWrap: "break-word" }}
+                    >{`${item?.from?.name} <${item?.from?.email}>`}</strong>
+                  </div>
+                }
+                subject={
+                  <div>
+                    Subject:{" "}
+                    <strong style={{ wordWrap: "break-word" }}>
+                      {item?.subject}
+                    </strong>
+                  </div>
+                }
+                shortDesc={
+                  item?.short_description ? (
+                    <div className="card-short-description">
+                      {emailCard
+                        ? item?.short_description?.substring(0, 48) + "..."
+                        : item?.short_description}
+                    </div>
+                  ) : null
+                }
+                date={<div>{item?.date ? formatDate(item?.date) : null}</div>}
+                emailCard={emailCard}
+                handleClick={handleClick}
+              />
+            ))
+          : null}
       </div>
-    </>
+      {emailCard ? <CardBody data={data} state={state} id={id} /> : null}
+    </div>
   );
 };
 
